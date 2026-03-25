9d57165143d11c0e92f925950cedbce4b43926b6
Harsh Pailkar <85762743+harshpailkar@users.noreply.github.com>
2026-03-25 10:33:16 +0530

    Refactor OrderBook class and clean up includes
    Removed unused includes and restructured OrderBook class.

--- lume.cpp
+++ lume.cpp
@@ -1,37 +1,4 @@
-#include <iostream>
-#include <map>
-#include <list>
-#include <unordered_map>
-#include <algorithm>
-
-struct Order {
-    uint64_t orderId;
-    bool isBuy;
-    int64_t price;
-    uint32_t quantity;
-};
-
-struct LimitLevel {
-    int64_t price;
-    uint64_t totalQuantity = 0;
-    std::list<Order> orders;
-};
-
-struct OrderBookEntry {
-    std::list<Order>::iterator it;
-    LimitLevel *level;
-};
-
-class OrderBook {
-private:
-    std::map<int64_t, LimitLevel, std::greater<int64_t>> bids;
-    std::map<int64_t, LimitLevel, std::less<int64_t>> asks;
-    std::unordered_map<uint64_t, OrderBookEntry> orderMap;
-
-public:
-    void addOrder(uint64_t orderId, bool isBuy, int64_t price, uint32_t quantity);
-    void cancelOrder(uint64_t orderId);
-};
+#include "lume.h"
 
 void OrderBook::addOrder(uint64_t orderId, bool isBuy, int64_t price, uint32_t quantity) {
     if (isBuy) {
